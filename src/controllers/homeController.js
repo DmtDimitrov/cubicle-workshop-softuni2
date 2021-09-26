1329a2f42fa40ecee4d800966422f26a31391dd2
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-09-27 01:51:33 +0300

    Add AboutView

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -6,6 +6,11 @@ const homeView = (req, res) => {
     res.render('index');
 };
 
+const aboutView = (req, res) => {
+    res.render('about');
+}
+
 router.get ('/', homeView);
+router.get ('/about', aboutView);
 
 module.exports = router;
