56cea83d23720de427f5228429d2cf4fa4862af8
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-05 12:04:38 +0300

    Update db.js

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,13 @@
 const mongoose = require('mongoose');
 
-function initDb(url) {
-    return mongoose.connect(url);
+// function initDb(url) {
+//     return mongoose.connect(url);
+// };
+
+async function initDb(url) {
+    await mongoose.connect(url);
+    console.log('Database connected');
 };
 
-// async function initDb(url){
-//     await mongoose.connect(url)
-// };
 
 module.exports = initDb;
