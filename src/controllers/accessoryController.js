22c40a5cfcb67187b4cb8e5db8d64c26b9df5db4
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-06 01:10:03 +0300

    add accessory service
    add functionality to create an accessory

--- src/controllers/accessoryController.js
+++ src/controllers/accessoryController.js
@@ -1,11 +1,25 @@
 const express = require('express');
 
+const accessoryService = require('../services/accessoryService.js');
+
 const router = express.Router();
 
 const createAccessoryView = (req, res) => {
     res.render('accessory/create');
 };
 
+const createAccessory = async (req, res) => {
+    let { name, description, imageUrl } = req.body;
+    try {
+        await accessoryService.create(name, description, imageUrl);
+        
+        res.redirect('/'); 
+    } catch (error) {
+        res.status(400).send(error.message);
+    }
+};
+
 router.get('/create', createAccessoryView);
+router.post('/create', createAccessory);
 
 module.exports = router;
