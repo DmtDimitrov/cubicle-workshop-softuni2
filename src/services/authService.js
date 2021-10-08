05d61496ceeaecd17490d2fba5866a02f091656d
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-08 19:58:17 +0300

    add register user with hashed pass

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,15 +1,19 @@
+const bcrypt = require('bcrypt');
+const User = require("../models/User.js");
 
-// const register = (username, password, repeatPassword) => {
+const register = (username, password, repeatPassword) => {
+    console.log(username, password, repeatPassword);
+    return bcrypt.hash(password, 9)
+        .then(hash => User.create({ username, password: hash }))
+};
+
+const authService = {
+    register,
+};
+
+module.exports = authService;
+
+
+// exports.register = function(username, password, repeatPassword){
 //     console.log(username, password, repeatPassword);
 // };
-
-// const authService = {
-//     register,
-// };
-
-// module.exports = authService;
-
-
-exports.register = function(username, password, repeatPassword){
-    console.log(username, password, repeatPassword);
-};
