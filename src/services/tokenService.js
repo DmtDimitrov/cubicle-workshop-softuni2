8243efce69379bb82b0769eb85069a9d786ab84b
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-09 04:11:19 +0300

    delete comment on token service

--- src/services/tokenService.js
+++ src/services/tokenService.js
@@ -1,19 +1,7 @@
-// const jwt = require('jsonwebtoken');
+
 const { SECRET } = require('../constants.js');
 const jwtUtils = require('../utils/jwtUtils.js');
 
-// function jwtSign(payload, secret) {
-//     let promise = new Promise((resolve, reject) => {
-//         jwt.sign(payload, secret, function (err, token) {
-//             if (err) {
-//                 reject(err);
-//             } else {
-//                 resolve(token);
-//             }
-//         });
-//     });
-//     return promise;
-// };
 
 const create = (user) => {
     // console.log(user);
